28d854c1e75ff66603d3d244ee9f24b46db5f6ff
adsdi <sbolt@adsdi.com>
2021-03-12 20:31:51 -0600

    Testing deployment to gitpages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>Databerry Customer Portal</h1>
+      <p>Test Project for Code Evaluation</p>
+      <p>April 2021</p>
     </div>
   );
 }
